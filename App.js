17f1ea8975cf28bd610c60594eb64ed197da540e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-21 16:31:51 -0600

    Finish the functioanlity of the rock, paper, scissors game

---
+++ App.js
@@ -0,0 +1,42 @@
+const prompt = require("prompt-sync")();
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+while (true) {
+  console.log("\n")
+  const playerChoice = prompt("Enter 'rock', 'paper', or 'scissors' (or 'q' to quit): ");
+  if (playerChoice.toLowerCase() === 'q') {
+    break;
+  }
+
+  if (playerChoice.toLowerCase() !== 'rock' && playerChoice.toLowerCase() !== 'paper' && playerChoice.toLowerCase() !== 'scissors') {
+    console.log("Please enter a valid choice.");
+    continue;
+  }
+  
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.round(Math.random() * 2);
+  const computerChoice = choices[randomIndex];
+  
+  console.log("The computer chose: " + computerChoice);
+  
+  if (computerChoice === playerChoice) {
+    console.log("Draw!");
+    ties++;
+  } else if (
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+    console.log("Won!");
+    wins++;
+  } else {
+    console.log("Lost!");
+    losses++;
+  }
+}
+
+console.log("\n")
+console.log("Wins:", wins, "Losses:", losses, "Ties:", ties);
